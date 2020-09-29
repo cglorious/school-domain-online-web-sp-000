957822a5b6364ed81a934f6cf9d33c03fe6d7911
Catherine Gloria <catherinegloria@gmail.com>
2020-09-29 21:05:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,13 @@
 class School
-  attr_reader :name, :roster
+  attr_reader :name, :roster, :student, :grade
 
   def initialize(name)
     @name = name
     @roster = {}
+  end
+
+  def add_student(student, grade)
+    @student =
   end
 
 end
